166ea3c999a6097efead5d116c0497a721c6343c
Ozzy Knox <ozzyknox@gmail.com>
2023-02-16 17:00:26 +0100

    Testing k8s cluster

--- Shopping/Shopping.Client/Program.cs
+++ Shopping/Shopping.Client/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddHttpClient("ShoppingAPIClient", client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5000/"); // Shopping.API url
+    client.BaseAddress = new Uri(builder.Configuration["ShoppingAPIUrl"]); // Shopping.API url
 });
 
 builder.Services.AddControllersWithViews();
